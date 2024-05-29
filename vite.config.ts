05288d419a2b5d7fdad0d640c7c90eba17f8f354
Alexandre LEFAY <alexandre.lefay@gmail.com>
2024-05-29 22:54:13 +0200

    Fix .env used in HttpClient for baseUrl

--- vite.config.ts
+++ vite.config.ts
@@ -2,50 +2,7 @@ import basicSsl from '@vitejs/plugin-basic-ssl';
 import react from '@vitejs/plugin-react';
 import { defineConfig, loadEnv } from 'vite';
 
-/** List of the app build environments (.env.development is not included)
- * CAUTION: The name must be exactly the same as the .env.environment associated file
- */
-const buildEnvironments = ['test', 'production'];
-
-// https://vitejs.dev/config/
-export default defineConfig(({ command, mode }) => {
-  /** When vite serve -> import.meta.env.DEV === true */
-  if (command === 'serve') {
-    /** Local development specific configuration */
-    return {
-      ...defaultConfiguration,
-      plugins: [...defaultConfiguration.plugins, basicSsl()],
-      css: {
-        modules: {
-          localsConvention: 'camelCaseOnly'
-        }
-      }
-    };
-  }
-  /** (Build command) Production and non-prod specific configuration -> import.meta.env.PROD === true */
-
-  /* Load env file based on `mode` in the current working directory.
-   * Example : Build running in test mode (pnpm build --mode test)
-   * Return all process.env variables prefixed with 'VITE_' by default
-   */
-
-  if (!buildEnvironments.includes(mode)) {
-    console.error(`Error: .env file for mode '${mode}' does not exist!`);
-    process.exit(1);
-  }
-
-  const env = loadEnv(mode, process.cwd());
-
-  return {
-    ...defaultConfiguration,
-    build: {
-      outDir: 'dist',
-      target: 'es2022',
-      sourcemap: env.VITE_GENERATE_SOURCEMAP === 'true'
-    }
-  };
-});
-
+// Default configuration for Vite
 const defaultConfiguration = {
   plugins: [
     react({
@@ -72,3 +29,43 @@ const defaultConfiguration = {
     }
   }
 };
+
+// Define the configuration based on the command and mode
+export default defineConfig(({ command, mode }) => {
+  // Load environment variables based on the mode
+  const env = loadEnv(mode, process.cwd(), '');
+
+  // Check if we are in development mode
+  if (command === 'serve') {
+    return {
+      ...defaultConfiguration,
+      plugins: [...defaultConfiguration.plugins, basicSsl()],
+      css: {
+        modules: {
+          localsConvention: 'camelCaseOnly'
+        }
+      },
+      define: {
+        'process.env': env
+      }
+    };
+  }
+
+  // Check if the mode is valid
+  if (!['test', 'production'].includes(mode)) {
+    console.error(`Error: .env file for mode '${mode}' does not exist!`);
+    process.exit(1);
+  }
+
+  return {
+    ...defaultConfiguration,
+    build: {
+      outDir: 'dist',
+      target: 'es2022',
+      sourcemap: env.VITE_GENERATE_SOURCEMAP === 'true'
+    },
+    define: {
+      'process.env': env
+    }
+  };
+});
