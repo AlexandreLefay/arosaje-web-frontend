5d317b4226ffc6ac377164f5e63bc912c25ad079
Alexandre LEFAY <alexandre.lefay@gmail.com>
2024-06-16 00:12:33 +0200

    Config: Fix Lint

--- src/App.tsx
+++ src/App.tsx
@@ -6,22 +6,16 @@ import './App.scss';
 import { StoreProvider } from '@contexts/StoreContext';
 import { rootStore } from '@stores/global/RootStore';
 import { AppRouter } from '@routes/AppRoutes';
-import config from '../auth_config.json';
-import { Auth0Provider } from '@auth0/auth0-react';
 
 /**
  * Component used to handle the application
  */
 export function App() {
-  const { domain, clientId, redirectUri } = config;
-
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
       <StoreProvider value={rootStore}>
-        <Auth0Provider domain={domain} clientId={clientId} redirectUri={redirectUri}>
-          <AppRouter />
-        </Auth0Provider>
+        <AppRouter />
       </StoreProvider>
     </ThemeProvider>
   );
